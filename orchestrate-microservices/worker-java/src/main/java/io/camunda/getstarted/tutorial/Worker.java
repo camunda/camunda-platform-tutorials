1bb531caa3aa05ce44063db3771ed06f8a5d443d
Bernd Ruecker <bernd.ruecker@camunda.com>
2023-10-24 16:46:30 +0200

    Removed deprecated @EnableZeebeClient

--- orchestrate-microservices/worker-java/src/main/java/io/camunda/getstarted/tutorial/Worker.java
+++ orchestrate-microservices/worker-java/src/main/java/io/camunda/getstarted/tutorial/Worker.java
@@ -8,10 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.camunda.zeebe.client.api.response.ActivatedJob;
-import io.camunda.zeebe.spring.client.EnableZeebeClient;
 
 @SpringBootApplication
-@EnableZeebeClient
 public class Worker {
 
   public static void main(String[] args) {
