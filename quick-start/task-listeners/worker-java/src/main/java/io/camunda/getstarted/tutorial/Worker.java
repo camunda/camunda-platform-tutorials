b0a0265970c30b0bc694e0e0d61ae5459bbb4564
Nico Korthout <nico.korthout@camunda.com>
2025-06-26 15:51:53 +0200

    Copy Microservices guide as task listener guide
    Rather than starting from scratch, I'll reuse what we build/wrote for
    the Microservices quick start guide.
    
    This just copies all files, so the changes become clear in later
    commits.

---
+++ quick-start/task-listeners/worker-java/src/main/java/io/camunda/getstarted/tutorial/Worker.java
@@ -0,0 +1,31 @@
+package io.camunda.getstarted.tutorial;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+
+@SpringBootApplication
+public class Worker {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Worker.class, args);
+  }
+  
+  @JobWorker(type = "orchestrate-something")
+  public Map<String, Object> orchestrateSomething(final ActivatedJob job) {
+
+      // Do the business logic
+      System.out.println("Yeah, now you can orchestrate something :-) You could use data from the process variables: " + job.getVariables());
+
+      // Probably add some process variables
+      HashMap<String, Object> variables = new HashMap<>();
+      variables.put("resultValue1", 42);
+      return variables;
+  }
+
+}
